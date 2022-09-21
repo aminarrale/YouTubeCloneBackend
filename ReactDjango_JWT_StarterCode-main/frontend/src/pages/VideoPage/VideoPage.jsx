37dde5e3dafd309a0b3e344edbbbb1603e15544f
Karim <aarrale12@gmail.com>
2022-09-20 19:41:35 -0500

    made video player with iframe tag

--- ReactDjango_JWT_StarterCode-main/frontend/src/pages/VideoPage/VideoPage.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/pages/VideoPage/VideoPage.jsx
@@ -1,44 +1,13 @@
-import React, { useEffect } from 'react';
 import React, { useState } from 'react';
-import useAuth from '../../hooks/useAuth';
+import { useParams } from 'react-router-dom';
 
 
-const VideoPage = (props) => {
-
-    const videoId = useParams();
-
-    useEffect(()=>{
-        console.log('whole object:',props.selectedVideo)
-    },[])
-
-    useEffect(() => {
-        props.getVideoComments(videoId)
-        props.getRelatedVideos(videoId)
-    },[props.selectedVideo])
-
-    useEffect(() => {
-        window.scrollTo(0, 0)
-      }, [videoId])
-
-    return ( 
-        <div className="container">
-            <div className="row">
-                <div className="col-sm-9">
-
-                        <VideoPlayer videoId={props.selectedVideo.id.videoId} />
-
-                    <div className="description">{props.selectedVideo.snippet.description}</div>
-                    <div>
-                        <CommentList videoId={props.selectedVideo.id.videoId} comments={props.comments} getVideoComments={props.getVideoComments}></CommentList>
-                    </div>
-                </div>
-                <div className="col-sm-3">
-                    <RelatedVideos relatedVideos={props.searchedVideos} getRelatedVideos={props.getRelatedVideos} pickVideo={props.pickVideo} submitVideoInfo={props.submitVideoInfo} ></RelatedVideos>
-                </div>
-            </div>
-
+const VideoPlayer = (props) => {
+    return(
+        <div className='video-player'>
+            <iframe style={{height:"60vh", width:"60 vw"}} className="iframe" src={`https://www.youtube.com/embed/${props.videoId}`} frameBorder="0"></iframe>
         </div>
-     );
-}
- 
-export default VideoPage;
+    )
+} 
+
+export default VideoPlayer
